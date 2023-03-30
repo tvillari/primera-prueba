c845557660f8b53139b0c1f3240a558986d3a220
tvillari <tvillari@leafnoise.io>
2023-03-30 12:59:53 -0300

    Cambios al site, agregue funciones y pagina de js

---
+++ index.js
@@ -0,0 +1,13 @@
+alert("bienvenidos!");
+
+function abrirMenu() {
+    document.getElementById("menu").style.display = "block";
+    console.log("aaaaaaaa")
+}
+function cerrarMenu() {
+    document.getElementById("menu").style.display = "none";
+    console.log("eeeeee")
+}
+function saludar() {
+    alert("hola");
+}
